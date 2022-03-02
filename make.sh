1f7b6b99c18afe4045f8a06a88c84ffc81977e1a
Mucheru <mucheru@outlook.com>
2022-03-02 09:27:27 +0300

    Updating Pipeline

--- make.sh
+++ make.sh
@@ -37,40 +37,40 @@ install-eksctl() {
         log skip eksctl already installed
     fi
 }
-# create the EKS cluster
-cluster-create() {
-    # check if cluster already exists (return something if the cluster exists, otherwise return nothing)
-    local exists=$(aws eks describe-cluster \
-        --name $PROJECT_NAME \
-        --profile $AWS_PROFILE \
-        --region $AWS_REGION \
-        2>/dev/null)
+# # create the EKS cluster
+# cluster-create() {
+#     # check if cluster already exists (return something if the cluster exists, otherwise return nothing)
+#     local exists=$(aws eks describe-cluster \
+#         --name $PROJECT_NAME \
+#         --profile $AWS_PROFILE \
+#         --region $AWS_REGION \
+#         2>/dev/null)
         
-    [[ -n "$exists" ]] && { error abort cluster $PROJECT_NAME already exists; return; }
+#     [[ -n "$exists" ]] && { error abort cluster $PROJECT_NAME already exists; return; }
 
-    # create a cluster named $PROJECT_NAME
-    log create eks cluster $PROJECT_NAME
+#     # create a cluster named $PROJECT_NAME
+#     log create eks cluster $PROJECT_NAME
 
-    eksctl create cluster \
-        --name $PROJECT_NAME \
-        --region $AWS_REGION \
-        --managed \
-        --node-type t2.small \
-        --nodes 1 \
-        --profile $AWS_PROFILE
-}
+#     eksctl create cluster \
+#         --name $PROJECT_NAME \
+#         --region $AWS_REGION \
+#         --managed \
+#         --node-type t2.small \
+#         --nodes 1 \
+#         --profile $AWS_PROFILE
+# }
 
-# get the cluster ELB URL
-cluster-elb() {
-    kubectl get svc \
-        --namespace $PROJECT_NAME \
-        --output jsonpath="{.items[?(@.metadata.name=='website')].status.loadBalancer.ingress[].hostname}"
-}
+# # get the cluster ELB URL
+# cluster-elb() {
+#     kubectl get svc \
+#         --namespace $PROJECT_NAME \
+#         --output jsonpath="{.items[?(@.metadata.name=='website')].status.loadBalancer.ingress[].hostname}"
+# }
 
-# delete the EKS cluster
-cluster-delete() {
-    eksctl delete cluster \
-        --name $PROJECT_NAME \
-        --region $AWS_REGION \
-        --profile $AWS_PROFILE
-}
+# # delete the EKS cluster
+# cluster-delete() {
+#     eksctl delete cluster \
+#         --name $PROJECT_NAME \
+#         --region $AWS_REGION \
+#         --profile $AWS_PROFILE
+# }
