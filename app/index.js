7b18f653957b781c2b23ce483d20f4a083d72c57
Mucheru <mucheru@outlook.com>
2022-03-02 13:53:14 +0300

    Update the Pipeline

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
+const HOST = '0.0.0.0';
+const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -7,6 +9,9 @@ const server = http.createServer((req, res) => {
   res.end(msg);
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+// server.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}/`);
+// });
+
+server.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
